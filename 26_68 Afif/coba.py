4094808211031825993ff1ef4d2805e5a590245e
mohafif2009 <mohafif2009>
2026-09-20 10:25:28 +0700

    26_68AfifRobbefi

---
+++ 26_68 Afif/coba.py
@@ -0,0 +1,29 @@
+a = 12
+b = 13
+
+ab = a+b
+a = ab
+
+print(a)
+
+
+# print(a+b)
+
+
+
+jumlah = 3
+apel = 10000
+
+jumlah +=apel
+
+print(jumlah)
+mangga = 30000
+apel = 5
+
+hasil = mangga/apel
+# print = (hasil)
+
+durian = 15
+semangka = 12
+
+#print(durian/semangka)

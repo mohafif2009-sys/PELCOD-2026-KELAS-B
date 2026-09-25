f9d99bb6d2c1f05e6b1a80807f461a8cf443d898
mohafif2009-sys <mohafif2009@gmail.com>
2026-09-25 14:42:14 +0700

    afif

---
+++ afif py.py
@@ -0,0 +1,20 @@
+nama = "afif"
+umur = 18
+tinggi = 170
+angka_favorit = 26
+
+print("nama:", nama)
+print("umur:", umur , "tahun")
+print("tinggi:", tinggi, "cm")
+print("Angka_vaforit:" , angka_favorit)
+
+harga_pensil = 4 * 2000
+harga_buku = 2 * 5000
+total = harga_pensil + harga_buku
+
+print("total belanja: Rp" ,total)
+
+if angka_favorit % 2 == 0:
+    print("angka genap")
+else:
+    print("angak ganjil")
